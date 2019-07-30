b7661453148d902fdd2e31825f13465238033410
Adrian Sanjo <jesuisanonime@gmail.com>
2019-07-30 10:31:59 +0200

    y

--- languageParser.py
+++ languageParser.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import List, Optional, Tuple, Union
 
+from tokens import *
 from languageLexer import Position, Token
 from error import Error, InvalidSyntaxError
 
@@ -100,7 +101,7 @@ class LessThanNode(Node):
         self.rightNode: Node = rightNode
         self.exprNodes: List[Node] = exprNodes
 
-class GreatrThanNode(Node):
+class GreaterThanNode(Node):
     def __init__(self, startPos: Position, endPos: Position, leftNode: Node, rightNode: Node, exprNodes: List[Node]):
         super().__init__(startPos, endPos)
         self.leftNode: Node = leftNode
@@ -125,6 +126,7 @@ class Parser:
         self.tokens: List[Token] = tokens
         self.idx: int = -1
         self.token: Optional[Token] = None
+        self.nextToken: Optional[Token] = None
         self.advance()
     
     def advance(self):
@@ -133,22 +135,101 @@ class Parser:
             self.token = self.tokens[self.idx]
         else:
             self.token = None
+        if self.idx + 1 < len(self.tokens):
+            self.nextToken = self.tokens[self.idx + 1]
+        else:
+            self.nextToken = None
     
     def parseTokens(self) -> Tuple[Node, Optional[Error]]:
         return self.makeProgram()
     
     def makeProgram(self) -> Tuple[Node, Optional[Error]]:
+        nodes = []
+        startPos = self.token.startPos
+        idx = self.idx
         assign, err = self.makeAssign()
-        return Node(), None
+        while not err:
+            nodes.append(assign)
+            idx = self.idx
+            assign, err = self.makeAssign()
+        if err.type != "NotAssign":
+            return Node(), err
+        self.idx = idx - 1
+        self.advance()
+        basicExpr, err = self.makeBasicExpr()
+        if err:
+            return Node(), err
+        nodes.append(basicExpr)
+        endPos = self.token.endPos
+        return ProgramNode(startPos, endPos, nodes), None
     
     def makeAssign(self) -> Tuple[Node, Optional[Error]]:
-        return Node(), None
+        startPos = self.token.startPos
+        if self.token.type != TT_IDENTIFIER:
+            return Node(), InvalidSyntaxError(self.token.startPos, self.token.endPos, "Expected Identifier")
+        funcName = self.token.value
+        assert(isinstance(funcName, str))
+        self.advance()
+        
+        if self.token.type != TT_LPAREN:
+            return Node(), InvalidSyntaxError(self.token.startPos, self.token.endPos, "Expected '('")
+        self.advance()
+        
+        params = []
+        if self.token.type == TT_IDENTIFIER:
+            params.append(self.token.value)
+            self.advance()
+            while self.token.type == TT_COMMA:
+                self.advance()
+                if self.token.type != TT_IDENTIFIER:
+                    return Node(), InvalidSyntaxError(self.token.startPos, self.token.endPos, "Expected identifier")
+                params.append(self.token.value)
+                self.advance()
+        if self.token.type != TT_RPAREN:
+            return Node(), InvalidSyntaxError(self.token.startPos, self.token.endPos, "Expected ')'")
+        self.advance()
+        
+        if self.token.type != TT_EQUAL:
+            return Node(), Error(self.token.startPos, self.token.endPos, "NotAssign", "Expected '='")
+        self.advance()
+        
+        exprNodes, err = self.makeExprs()
+        if err:
+            return Node(), err
+        endPos = exprNodes[-1].endPos
+        assert(endPos is not None)
+        return AssignNode(startPos, endPos, funcName, params, exprNodes), None
     
-    def makeExprs(self) -> Tuple[Node, Optional[Error]]:
-        return Node(), None
+    def makeExprs(self) -> Tuple[List[Node], Optional[Error]]:
+        exprs = []
+        expr, err = self.makeExpr()
+        if err:
+            return [], err
+        while self.token.type == TT_COMMA:
+            self.advance()
+            expr, err = self.makeExpr()
+            if err:
+                return [], err
+        exprs.append(expr)
+        return exprs, None
     
     def makeExpr(self) -> Tuple[Node, Optional[Error]]:
-        return Node(), None
+        if self.token.type == TT_IDENTIFIER and self.nextToken.type == TT_EQUAL:
+            startPos = self.token.startPos
+            varName = self.token.value
+            assert(isinstance(varName, str))
+            self.advance()
+            self.advance()
+            basicExpr, err = self.makeBasicExpr()
+            if err:
+                return Node(), err
+            endPos = basicExpr.endPos
+            assert(endPos is not None)
+            return VarAssignNode(startPos, endPos, varName, basicExpr), None
+        basicExpr, err = self.makeBasicExpr()
+        if err:
+            return Node(), err
+        return basicExpr, None
     
     def makeBasicExpr(self) -> Tuple[Node, Optional[Error]]:
         return Node(), None
