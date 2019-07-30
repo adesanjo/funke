0ca09e004f3777b0733677eed4e18590ae3242bd
Adrian Sanjo <jesuisanonime@gmail.com>
2019-07-30 14:08:25 +0200

    y

--- values.py
+++ values.py
@@ -3,14 +3,22 @@ from typing import Optional, Union
 from error import Position
 
 class Value:
-    def __init__(self, startPos: Optional[Position] = None, endPos: Optional[Position] = None, value: Union[int,float, str, None] = None):
+    def __init__(self, value: Union[int,float, str, None] = None, startPos: Optional[Position] = None, endPos: Optional[Position] = None):
+        self.value: Union[int, float, str, None] = value
         self.startPos: Optional[Position] = startPos
         self.endPos: Optional[Position] = endPos
-        self.value: Union[int, float, str, None] = value
     
     def __repr__(self) -> str:
         return str(self.value)
 
 class IntValue(Value):
-    def __init__(self, startPos: Optional[Position] = None, endPos: Optional[Position] = None, value: int = 0):
-        super().__init__(startPos, endPos, value)
+    def __init__(self, value: int = 0, startPos: Optional[Position] = None, endPos: Optional[Position] = None):
+        super().__init__(value, startPos, endPos)
+
+class FloatValue(Value):
+    def __init__(self, value: float = 0.0, startPos: Optional[Position] = None, endPos: Optional[Position] = None):
+        super().__init__(value, startPos, endPos)
+
+class StringValue(Value):
+    def __init__(self, value: str = "", startPos: Optional[Position] = None, endPos: Optional[Position] = None):
+        super().__init__(value, startPos, endPos)
